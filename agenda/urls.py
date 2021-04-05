90182acbcf046712d2169e41bf05b138c3a0c140
joaotaraujo <teixeira.araujo@gmail.com>
2021-04-05 12:02:39 -0300

    Finalizando o projeto.

--- agenda/urls.py
+++ agenda/urls.py
@@ -21,6 +21,13 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
-    path('', RedirectView.as_view(url='/agenda/'))
+    path('agenda/lista/', views.json_lista_eventos),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
+    path('', RedirectView.as_view(url='/agenda/')),
     #path('', views.index)
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
